62109981d281f6224fe573962e4b071b7a99eaff
Михаил Остромогольский <m.ostromogolskii@vk.team>
2022-09-13 08:15:07 +0300

    [lesson-1.7] Upgrade preparing script

--- create_lesson_template.sh
+++ create_lesson_template.sh
@@ -15,8 +15,11 @@ then
 else
     group=$tmp
 fi
-echo -e "\033[32mEnter lesson template name\033[0m"
-read tmp_name
+echo -e "\033[32mEnter lesson block number\033[0m"
+read block_number
+echo -e "\033[32mEnter lesson number\033[0m"
+read lesson_number
+tmp_name="lesson-${block_number}.${lesson_number}"
 echo -e "\033[32mStarting create lesson \033[35m${tmp_name}\033[32m in cource section \033[35m${group}\033[0m"
 git checkout master 
 git pull
@@ -33,10 +36,12 @@ done
 echo -e "\033[32mChecking terraform installation\033[0m"
 cd ${group}/${tmp_name}/terraform
 source $root_dir/infra/terraform/.env.sh
-terraform init && terraform plan
+terraform init > /dev/null && terraform plan > /dev/null
 if [ $? != 0 ] 
 then 
     echo -e "\033[31mTerraform planning failed\033[0m"
+    exit -1
 fi
+echo -e "\033[32mTerraform init and plan - OK!\033[0m"
 echo -e "\033[32mCreating ${tmp_name}.md file\033[0m"
 touch  $root_dir/$group/$tmp_name/$(echo $tmp_name | tr "." "-").md
